6c1fff8c26be9ea6da600c2cbd2f9dae93a8d1d0
Mike <106514673+Qnieq@users.noreply.github.com>
2022-10-29 19:55:50 +0300

    Add files via upload

---
+++ Aiogrambot_test.py
@@ -0,0 +1,9 @@
+from aiogram import executor
+from bot_cfg import dp
+from handlers import client
+
+client.register_hadlers_cl(dp)
+
+if __name__ == '__main__':
+    print('start')
+    executor.start_polling(dp, skip_updates=True)
---
+++ bot_cfg.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher
+
+
+API_TOKEN = 'BOT_TOKEN'
+PAYMENT_TOKEN = "PAYMENT_TOKEN"
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
