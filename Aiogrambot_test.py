356966659257d228c7f0bb835f1108ad88f95943
Mike <106514673+Qnieq@users.noreply.github.com>
2022-10-29 20:09:19 +0300

    Delete Aiogrambot_test.py

--- Aiogrambot_test.py
+++
@@ -1,9 +0,0 @@
-from aiogram import executor
-from bot_cfg import dp
-from handlers import client
-
-client.register_hadlers_cl(dp)
-
-if __name__ == '__main__':
-    print('start')
-    executor.start_polling(dp, skip_updates=True)
